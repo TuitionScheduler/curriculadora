4cd1f2c261cc7871363cdc2c6f8b7260dd2f7289
NorelyT <norely.torres1@upr.edu>
2025-03-26 13:14:17 -0400

    Modifying script to delete all DB data

--- CurriculadoraDB/CurriculadoraScript.sql
+++ CurriculadoraDB/CurriculadoraScript.sql
@@ -2,19 +2,19 @@ DROP TABLE IF EXISTS Course;
 
 DROP TABLE IF EXISTS Degree;
 
-CREATE TABLE Course (
-    course_id INTEGER PRIMARY KEY,
-    course_code TEXT NOT NULL
-);
+-- CREATE TABLE Course (
+--     course_id INTEGER PRIMARY KEY,
+--     course_code TEXT NOT NULL
+-- );
 
-CREATE TABLE Degree (
-    degree_id INTEGER PRIMARY KEY,
-    degree_title TEXT NOT NULL
-);
+-- CREATE TABLE Degree (
+--     degree_id INTEGER PRIMARY KEY,
+--     degree_title TEXT NOT NULL
+-- );
 
-INSERT INTO Course (course_code) VALUES ('DUMM3101');
+-- INSERT INTO Course (course_code) VALUES ('DUMM3101');
 
-INSERT INTO Degree (degree_title) VALUES ('Dummy');
+-- INSERT INTO Degree (degree_title) VALUES ('Dummy');
 
 --SELECT course_code FROM Course;
 --SELECT degree_title FROM Degree;
