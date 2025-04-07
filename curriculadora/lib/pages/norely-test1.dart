3cbbcc768129fdeab8838e15e8c65d3963653f53
NorelyT <norely.torres1@upr.edu>
2025-04-06 22:37:07 -0400

    Added storage support for mobile devices (Android and IOS), database setup on app startup, and more querying functions for the database. (Still needs testing) 1. Added "storeDatabaseInDeviceStorage()" function for database storage on mobile devices 2. Added querying for mobile devices 3. Added "getAllRecordsFromDB()", "getAllRecordsFromDBColumn()" and "getDataFromDB()" functions for querying to the database 4. Added "setupLocalStorage()" function for checking, downloading and storing courses.db on persistent storage

--- curriculadora/lib/pages/norely-test1.dart
+++ curriculadora/lib/pages/norely-test1.dart
@@ -2,70 +2,45 @@
 
 import 'package:flutter/material.dart';
 import 'package:url_launcher/link.dart';
-
-// Packages for extracting data from Github 
-import 'package:http/http.dart' as http;
-import 'package:path_provider/path_provider.dart'; 
-import 'dart:io';
-// import 'dart:async';
-import 'package:sqflite/sqflite.dart';
-
-downloadDatabaseFromGithub() {
-  String rawUrl = 'https://raw.githubusercontent.com/TuitionScheduler/curriculadora/curriculum-form/data/database/courses.db'
-  dbUri = await http.get(Uri.parse(rawUrl));
-  String dbFilePath = '${Directory.current.path}/frontend_courses.db';
-  dbFile = File(dbFilePath);
-  await dbFile.writeAsBytes(dbUri.bodyBytes);
-
-  return dbFile;
-}
-
-// Perform query
-extractDataFromDatabase() {
-  File dbFile = await downloadDatabaseFromGithub();
-  String dbPath = dbFile.path;
-  Database database = await openDatabase(dbPath);
-  List<Map<String, dynamic>> result = await database.rawQuery
-  ('SELECT * FROM users');
-  for (var row in result) {
-    print(row); 
-  }
-  await database.close();
-}
-
-
-
-
-
-
-
-
-
-
-
+// ------------- For .db file management, storage and queries ------------------
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:typed_data';
 import 'package:indexed_db/indexed_db.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi_web.dart';
 import 'package:flutter/foundation.dart';
+import 'dart:io';
+import 'dart:io' show Platform;
+import 'package:sqflite/sqflite.dart';
 
 // Downloads the SQLite .db file from GitHub using its raw url
 Future<Uint8List> downloadDatabaseFromGithub() async {
-  String rawUrl = 'https://raw.githubusercontent.com/TuitionScheduler/curriculadora/curriculum-form/data/database/courses.db'
-  http_response = await http.get(Uri.parse(rawUrl));
+  String rawUrl = 'https://raw.githubusercontent.com/TuitionScheduler/curriculadora/curriculum-form/data/database/courses.db';
+  http.Response response = await http.get(Uri.parse(rawUrl));
   
-  if (http_response.statusCode == 200) {
-    return http_response.bodyBytes; // Return the raw bytes of the .db file
+  if (response.statusCode == 200) {
+    return response.bodyBytes; // Return the raw bytes of the .db file
   } else {
     throw Exception('Could not download the .db file from Github');
   }
 }
 
+
+// Stores courses.db in the device's "\Databases" directory
+// For Android and IOS 
+Future<void> storeDatabaseInDeviceStorage(Uint8List databaseBytes) async {
+  String pathToDatabaseStorage = await getDatabasesPath();
+  String dbFilePath = '$pathToDatabaseStorage/courses.db';
+  File dbFile = File(dbFilePath);
+  await dbFile.writeAsBytes(databaseBytes);
+}
+
+
 // Creates an IndexedDB that stores courses.db 
+// For web platforms (browsers)
 Future<void> storeDatabaseInIndexedStorage(Uint8List databaseBytes) async {
   // Creates an IndexedDB and a general ObjectStore to store files in
-  indexedStorage = await window.indexedDB!.open("User_Indexed_Storage", version: 1, onUpgradeNeeded: (e) {
+  IDBDatabase indexedStorage = await window.indexedDB!.open("User_Indexed_Storage", version: 1, onUpgradeNeeded: (e) {
     temp = e.target.result as IDBDatabase;
     if (!temp.objectStoreNames.contains("local_files")) {
       temp.createObjectStore("local_files");
@@ -74,35 +49,37 @@ Future<void> storeDatabaseInIndexedStorage(Uint8List databaseBytes) async {
   });
 
   // Stores the database in the ObjectStore
-  store = indexedStorage.transaction("local_files", "readwrite").objectStore("local_files");
+  ObjectStore store = indexedStorage.transaction("local_files", "readwrite").objectStore("local_files");
   Blob databaseFile = Blob([databaseBytes]);
   await store.put(databaseFile, "courses.db");
   // do i need to close???
 }
 
-// Opens database and configures setup for queries
-databaseSetup() {
-  final indexedStorage = await window.indexedDB.open("User_Indexed_Storage", version: 1);
-  final store = indexedStorage.transaction("local_files", "readonly").objectStore("local_files");
+
+// Opens the database and configures setup for queries
+Database querySetupForWeb() {
+  IDBDatabase indexedStorage = await window.indexedDB.open("User_Indexed_Storage", version: 1);
+  ObjectStore store = indexedStorage.transaction("local_files", "readonly").objectStore("local_files");
   Blob databaseFile = await store.get("courses.db");
   
   // Sets up database factory for SQLite queries
   if (databaseFile != null) {
-    final dbFileBytes = await databaseFile.arrayBuffer();
+    ArrayBuffer dbFileBytes = await databaseFile.arrayBuffer();
     Uint8List dbFileUint8List = Uint8List.view(dbFileBytes);
 
     // final fileBytes = await dbFile!.slice(0, dbFile.size).arrayBuffer();
     // final byteData = ByteData.sublistView(fileBytes as Uint8List);
     
-    var factory = databaseFactoryFfiWeb;
-    var database = await factory.openDatabaseFromBytes(dbFileUint8List);
-    return database
+    DatabaseFactoryFfiWeb factory = databaseFactoryFfiWeb;
+    Database database = await factory.openDatabaseFromBytes(dbFileUint8List);
+    return database;
   }
   else {
     await database.close();
     throw Exception('No database found in IndexedStorage.');
   }
 }
+
 
 // Future<void> queryDatabase(int course) async {
 //     // Query the database
@@ -116,66 +93,148 @@ databaseSetup() {
 //   }
 // }
 
-// Queries the database for all programs' data records with all their fields (columns)
-Future<void> getAllPrograms(database) async {
+
+// Queries the database for all the data records within a specified table (with all their fields (columns))
+List<Map<String, dynamic>> getAllRecordsFromDB(String table_name) async {
   if (kIsweb){
-    database = databaseSetup()
-    List<Map<String, dynamic>> programs = await database.rawQuery('SELECT * FROM programs');
-    await database.close();
+    Database database = querySetupForWeb();
+  }
+  else if (Platform.isAndroid OR Platform.isIOS) {
+    String pathToDatabaseStorage = await getDatabasesPath();
+    Database database = await openDatabase('$pathToDatabaseStorage/courses.db');
+  }
+
+  List<Map<String, dynamic>> result = await database.rawQuery('SELECT * FROM $table_name');
+  await database.close();
+  if (result.isNotEmpty) {
+    return result;
+  }
+  else {
+    print('Table $table_name not found in courses.db');
   }
 }
 
 
+// Queries the database for all the data records within a specified column 
+List<Map<String, dynamic>> getAllRecordsFromDBColumn(String table_name, String column_name) async {
+  if (kIsweb){
+    Database database = querySetupForWeb();
+  }
+  else if (Platform.isAndroid OR Platform.isIOS) {
+    String pathToDatabaseStorage = await getDatabasesPath();
+    Database database = await openDatabase('$pathToDatabaseStorage/courses.db');
+  }
+
+  List<Map<String, dynamic>> result = await database.rawQuery('SELECT $column_name FROM $table_name');
+  await database.close();
+  if (result.isNotEmpty) {
+      return result;
+    }
+  else {
+    print('Column $column_name not found in $table_name table');
+  }
+}
 
 
+// Queries the database for a specific data element 
+List<Map<String, dynamic>> getDataFromDB(String table_name, String column_name, String data) async {
+  if (kIsweb){
+    Database database = querySetupForWeb();
+  }
+  
+  else if (Platform.isAndroid OR Platform.isIOS) {
+    String pathToDatabaseStorage = await getDatabasesPath();
+    Database database = await openDatabase('$pathToDatabaseStorage/courses.db');
+  }
+
+  List<Map<String, dynamic>> result = await database.rawQuery(
+    'SELECT * FROM $table_name 
+    WHERE $column_name = ?', [data]);
+  
+  await database.close();
+  if (result.isNotEmpty) {
+    return result;
+  }
+  else {
+    print('$column_name : $data not found in $table_name table');
+  }
+}
 
 
+class UpdateProgress extends StatefulWidget {
+  const UpdateProgress({super.key});
+
+  @override
+  State<UpdateProgress> createState() => _UpdateProgressState();
+}
+
+class _UpdateProgressState extends State<UpdateProgress> {
+  @override
+  void initState() {
+    super.initState();
+    _setupLocalStorage();
+  }
+
+  // Download the database and store it in local storage when the app starts
+  Future<void> _setupLocalStorage() async {
+    // Check if the device is a web platform (browser) 
+    if (kIsweb) {
+      // Check if the database has already been downloaded and stored in IndexedStorage
+      try {
+        indexedStorage = window.indexedDB.open("User_Indexed_Storage", version: 1);
+      } catch (e) {
+        Uint8List databaseBytes = await downloadDatabaseFromGithub();
+        await storeDatabaseInIndexedStorage(databaseBytes);
+      }
+    }
+    // Check if the device is Android or IOS (mobile)
+    else if (Platform.isAndroid OR Platform.isIOS){
+      // Check if the database has already been downloaded and stored in the device's "/Databases" directory
+      String pathToDatabaseStorage = await getDatabasesPath();
+      if (!File('$pathToDatabaseStorage/courses.db').exists) {
+        Uint8List databaseBytes = await downloadDatabaseFromGithub();
+        await storeDatabaseInDeviceStorage(databaseBytes);
+      }
+    }    
+  }
 
 
-// class UpdateProgress extends StatefulWidget {
-//   const UpdateProgress({super.key});
-
-//   @override
-//   State<UpdateProgress> createState() => _UpdateProgressState();
-// }
-
-// class _UpdateProgressState extends State<UpdateProgress> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: <Widget>[
-//           const Text(
-//             'This is the Update Progress Page',
-//           ),
-//           const Text("(MOCKUP)"),
-//           const Text("Your current plan for the next semester is:\n"
-//               "Year: 2024, Term: Spring\n"
-//               "CIIC4151\n"
-//               "CIPO3011\n"
-//               "EDFU3012\n"
-//               "INME4045\n"
-//               "PSIC3002"),
-//           Link(
-//               uri: Uri.parse(
-//                   'https://matrical.site/?term=Spring&year=2024&courses=CIIC4151%2CCIPO3011%2CEDFU3012%2CINME4045%2CPSIC3002'), // hard-coded url
-//               target: LinkTarget.blank,
-//               builder: (context, followLink) => ElevatedButton(
-//                   onPressed: followLink,
-//                   child: const Text("Export to Matrical"))),
-//           const Divider(),
-//           Text("Is this still your plan?"),
-//           ElevatedButton(
-//               onPressed: () {},
-//               child: Text("Yes, continue with this semester")),
-//           SizedBox(
-//             height: 10,
-//           ),
-//           ElevatedButton(
-//               onPressed: () {}, child: Text("No, generate new sequence"))
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'This is the Update Progress Page',
+          ),
+          const Text("(MOCKUP)"),
+          const Text("Your current plan for the next semester is:\n"
+              "Year: 2024, Term: Spring\n"
+              "CIIC4151\n"
+              "CIPO3011\n"
+              "EDFU3012\n"
+              "INME4045\n"
+              "PSIC3002"),
+          Link(
+              uri: Uri.parse(
+                  'https://matrical.site/?term=Spring&year=2024&courses=CIIC4151%2CCIPO3011%2CEDFU3012%2CINME4045%2CPSIC3002'), // hard-coded url
+              target: LinkTarget.blank,
+              builder: (context, followLink) => ElevatedButton(
+                  onPressed: followLink,
+                  child: const Text("Export to Matrical"))),
+          const Divider(),
+          Text("Is this still your plan?"),
+          ElevatedButton(
+              onPressed: () {},
+              child: Text("Yes, continue with this semester")),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {}, child: Text("No, generate new sequence"))
+        ],
+      ),
+    );
+  }
+}
