dcca13519c8c5ec270f3ec9780c17c7f0696c902
Ian-Ortiz <ian.ortiz5@upr.edu>
2025-04-24 20:32:31 -0400

    Made mockups for Update Progress page, and the 2nd page of Add Curriculum

---
+++ curriculadora/lib/pages/add_curriculum_courses.dart
@@ -0,0 +1,96 @@
+import 'package:curriculadora/models/reading-test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+// class CourseStatus extends StatefulWidget {
+//   final String code;
+//   const CourseStatus({super.key, required this.code});
+//
+//   @override
+//   State<CourseStatus> createState() => _CourseStatusState();
+// }
+//
+// class _CourseStatusState extends State<CourseStatus> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return FormBuilderRadioGroup(name: widget.code, options: const [
+//       FormBuilderFieldOption(value: 'not-complete', child: Text("NO")),
+//       FormBuilderFieldOption(value: 'in-progress', child: Text("IP")),
+//       FormBuilderFieldOption(value: 'complete', child: Text("OK")),
+//     ]);
+//   }
+// }
+
+class AddCurriculumCourses extends StatefulWidget {
+  const AddCurriculumCourses({super.key});
+
+  @override
+  State<AddCurriculumCourses> createState() => _AddCurriculumCoursesState();
+}
+
+class _AddCurriculumCoursesState extends State<AddCurriculumCourses> {
+  final _formKey = GlobalKey<FormBuilderState>();
+  late Map<String, dynamic> formData = {};
+  // Map<String, Map<String, dynamic>> courseTree = testCourses();
+  Iterable<String> courses = testCourses().keys;
+
+  Widget courseStatus(String code) {
+    return FormBuilderRadioGroup(
+        name: code,
+        initialValue: 'not-complete',
+        options: const [
+          FormBuilderFieldOption(value: 'not-complete', child: Text("NO")),
+          FormBuilderFieldOption(value: 'in-progress', child: Text("IP")),
+          FormBuilderFieldOption(value: 'complete', child: Text("OK")),
+        ]);
+  }
+
+  Widget courseStatusForm(Iterable<String> courses) {
+    List<Widget> courseList = [];
+    for (final course in courses) {
+      courseList.add(Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: Text(course.toString())),
+          const Spacer(),
+          Expanded(child: courseStatus(course)),
+        ],
+      ));
+      courseList.add(const Divider());
+    }
+    courseList.removeLast();
+    return Container(
+        child: Padding(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: courseList,
+      ),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilder(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.onUnfocus,
+        child: SingleChildScrollView(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            // courseStatusForm(courses),
+            courseStatusForm(courses),
+
+            ElevatedButton(
+                onPressed: () {
+                  _formKey.currentState?.save();
+                  formData = _formKey.currentState!.value;
+                  ScaffoldMessenger.of(context).removeCurrentSnackBar();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(formData.toString())));
+                },
+                child: const Text("Save")),
+          ],
+        )));
+  }
+}
